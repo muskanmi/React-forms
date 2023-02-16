69b11ab882068068704fc345e9cc577901c11ec4
Muskan Mittal <mittalmuskan758@gmail.com>
2023-02-16 13:50:13 +0530

    source code added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Form from './Form';
+import FormTwo from './FormTwo';
+import FormThree from './FormThree';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <Form />
+  // <FormTwo />
+  <FormThree />
+
 );
 
 // If you want to start measuring performance in your app, pass a function
